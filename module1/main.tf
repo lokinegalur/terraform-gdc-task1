309f91b69655112a812bd2a84e308f0f0fe31435
lokinegalur <negalurlohith@gmail.com>
2022-05-16 14:23:04 +0530

    Initial Commit

---
+++ module1/main.tf
@@ -0,0 +1,35 @@
+data "template_file" "file" {
+  template = "${file(var.script_path)}"
+}
+
+
+resource "google_compute_instance" "default" {
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.zone
+
+  
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      nat_ip = var.static_ip
+    }
+  }
+
+  metadata = {
+    name = "debian-test"
+  }
+
+  metadata_startup_script = "${data.template_file.file.rendered}"
+
+ 
+}
